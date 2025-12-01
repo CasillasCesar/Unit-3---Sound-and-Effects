24bdd90875fa6e265c5b999203b3460fa165d6f7
CasillasCesar <78441226+CasillasCesar@users.noreply.github.com>
2025-12-01 16:19:33 -0600

    Make player jump at start

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,10 +2,12 @@ using UnityEngine;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
+    private Rigidbody playerRb;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody>();
+        playerRb.AddForce(Vector3.up * 100);
     }
 
     // Update is called once per frame
